e13ae6bd8bf515d45b84ec0819ba7e2ce57f3009
Brian Gomez <brian-devops@Brians-MacBook-Pro.local>
2023-09-15 19:53:46 -0400

    Added Login Page, Login Controller with UserLogin and ForgotPassword functions, and Forgot Password Page

---
+++ InventoryManagementSystem/Controllers/UserController.cs
@@ -0,0 +1,31 @@
+﻿using InventoryManagementSystem.Models;
+using InventoryManagementSystem.Repository.abstraction;
+using Microsoft.AspNetCore.Mvc;
+using System.Diagnostics;
+
+namespace InventoryManagementSystem.Controllers
+{
+    public class UserController : Controller
+    {
+        private readonly ILogger<UserController> _logger;
+        private readonly IFiberspaceRepository _repository;
+
+        public UserController(ILogger<UserController> logger, IFiberspaceRepository repository)
+        {
+            _logger = logger;
+            _repository = repository;
+        }
+
+        public IActionResult Index(string email)
+        {
+            ViewBag.Email = email;
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
